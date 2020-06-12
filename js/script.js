fc5e64f5ae26fd314bebd33fdfd20d39cec11cd0
heather <heathermgong@gmail.com>
2020-06-12 00:47:02 -0700

    added hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 console.log("Welcome to the my portfolio website!");
+
+function menuToggle() {
+  var nav = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle'){
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+function menuContactToggle() {
+    var x = document.getElementById('myNavtoggle');
+    console.log('TCL: menuToggle -> x', x);
+    if (x.className === 'navtoggle') {
+    x.className += ' ';
+    } else {
+    x.className = 'navtoggle';
+    }
+}
