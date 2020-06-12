bad7ac3a696c665f309b785ee4695d718aae7270
heather <heathermgong@gmail.com>
2020-06-12 16:24:08 -0700

    changed script to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 console.log("Welcome to the my portfolio website!");
 
 function menuToggle() {
-  var nav = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle'){
-    x.className += ' responsive';
-  } else {
-    x.className = 'navtoggle';
-  }
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
 }
+
 function menuContactToggle() {
     var x = document.getElementById('myNavtoggle');
-    console.log('TCL: menuToggle -> x', x);
     if (x.className === 'navtoggle') {
-    x.className += ' ';
+      x.className += 'responsive';
     } else {
     x.className = 'navtoggle';
     }
